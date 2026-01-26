3a9192eac6a026510df70ee5537c64d5da3e0bfe
Marcin Barć <marcin.barc.programowanie@gmail.com>
2026-01-26 18:59:46 +0100

    Reset from api test:5

--- src/main/java/com/usermanagmentbackend/mail/MailConfiguration.java
+++ src/main/java/com/usermanagmentbackend/mail/MailConfiguration.java
@@ -4,10 +4,16 @@ import com.usermanagmentbackend.config.AppProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 
 @Configuration
 public class MailConfiguration {
+
+	public MailConfiguration(final Environment env) {
+		System.out.println("app.mail.enabled = " + env.getProperty("app.mail.enabled"));
+		System.out.println("MAIL_ENABLED = " + env.getProperty("MAIL_ENABLED"));
+	}
 
 	@Bean
 	@ConditionalOnProperty(prefix = "app.mail", name = "enabled", havingValue = "true")
