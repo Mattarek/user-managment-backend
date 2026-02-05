286d4f875acdf05c91305afb83f8df3114adbe0b
Marcin Barć <marcin.barc.programowanie@gmail.com>
2026-02-05 16:18:33 +0100

    Allow for all /apu/auth

--- src/main/java/com/usermanagmentbackend/config/SecurityConfig.java
+++ src/main/java/com/usermanagmentbackend/config/SecurityConfig.java
@@ -28,12 +28,7 @@ public class SecurityConfig {
 				.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers(
-								"/api/auth/register",
-								"/api/auth/login",
-								"/api/auth/refresh-token",
-								"/api/auth/logout",
-								"/api/auth/reset-password",
-								"/api/auth/reset-password",
+								"/api/auth/*",
 								"/actuator/health/**",
 								"/actuator/info"
 						).permitAll()
